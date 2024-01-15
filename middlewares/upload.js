6dfd71304ad7f6057ad824ebb421fb9928c850ca
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2024-01-15 15:04:56 +0200

    end

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -15,36 +15,15 @@ const storage = multer.diskStorage({
     }
 })
 
-// const limits = {
-//     fileSize: 250 * 250 * 5,
-// };
 
-// const fileFilter = (req, file, callback)=> {
-//     const extention = req.originalname.split(".").pop();
-//     if(extention === "exe") {
-//         callback(HttpError(400, ".exe not valid extention"));
-//     }
-// }
 
 const upload = multer({
     storage,
-    // limits,
-    // fileFilter,
+    
 })
 
 
-// const destination = path.resolve("tmp");
 
-// const storage = multer.diskStorage({
-//   destination,
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-// });
 
 
 module.exports = upload;
